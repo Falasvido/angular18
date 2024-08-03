306b65d027f8a5c1562dd73f718bbe819d0bdffd
Jaume Güell Sabaté <j.guell.sabate@gmail.com>
2024-08-03 17:52:50 +0200

    signals, control flow and other things

---
+++ src/services/characters.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Character, CharacterInfo } from '../models/character.model';
+import { CharacterAdapter } from '../adapters/character.adapter';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharactersService {
+
+  private mainUrl = 'https://rickandmortyapi.com/api/';
+  private charactersUrl = this.mainUrl + 'character';
+
+  constructor(private http: HttpClient) { }
+
+  getCharacters(): Observable<Character[]> {
+    return this.http.get<CharacterInfo>(this.charactersUrl).pipe(map((result: CharacterInfo) => CharacterAdapter(result)));
+  }
+
+  getCharacterInformation(url: string): Observable<Character> {
+    return this.http.get<Character>(url);
+  }
+}
